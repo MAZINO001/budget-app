4a46c4679ec99a8662a11a9de43188440fb8d67c
mounir <marnissimounir05@gmail.com>
2024-03-20 23:41:19 +0100

    fixing error

--- budget/src/App.jsx
+++ budget/src/App.jsx
@@ -1,5 +1,11 @@
+import Layout from "./components/scene/Layout";
+import TopBar from "./components/global/TopBar";
+console.log("App rendered");
 function App() {
-  <div className="text-white">hi this is mounir</div>;
+  <div className="App text-white">
+    <TopBar />
+    <Layout />
+  </div>;
 }
 
 export default App;
--- budget/src/components/scene/Layout.jsx
+++ budget/src/components/scene/Layout.jsx
@@ -1,9 +1,10 @@
 import Dashboard from "../Dashboard/Dashboard";
 import Sidebar from "../global/Sidebar";
 
-export default function scene() {
+export default function Layout() {
   return (
     <div className="flex">
+      hi this is layout 
       <Dashboard/>
       <Sidebar/>
     </div>
