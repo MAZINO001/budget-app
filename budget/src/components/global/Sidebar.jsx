0fa4b4d3ae88401dcdfe8927bc8ff354313d8ed8
mounir <marnissimounir05@gmail.com>
2024-03-21 17:44:37 +0100

    making sidebar

--- budget/src/components/global/Sidebar.jsx
+++ budget/src/components/global/Sidebar.jsx
@@ -1,3 +1,20 @@
+import { MdOutlineAddBox } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
+import { MdHistory } from "react-icons/md";
+import { IoMdCalendar } from "react-icons/io";
+import { MdHomeFilled } from "react-icons/md";
+import { GiWallet } from "react-icons/gi";
 export default function Sidebar() {
-  return <div className="bg-blue-500 w-[12vw] h-[100vh]">Sidebar</div>;
+  return (
+    <div className=" w-[10vw] h-[100vh] flex flex-col justify-between items-center">
+      <GiWallet className="text-3xl mt-2 cursor-pointer" />
+      <div className="flex flex-col justify-between h-[400px]  text-2xl cursor-pointer mb-16">
+        <MdHomeFilled className="" />
+        <MdOutlineAddBox className="" />
+        <MdHistory className="" />
+        <IoMdCalendar className="" />
+      <IoMdSettings className="" />
+      </div>
+    </div>
+  );
 }
