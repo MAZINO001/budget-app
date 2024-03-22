59b97d840a5609b773aa768c9c35f641eb35a514
mounir <marnissimounir05@gmail.com>
2024-03-22 18:47:13 +0100

    add responsive

--- budget/src/components/Dashboard/Dashboard.jsx
+++ budget/src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,23 @@
 export default function Dashboard() {
   return (
-    // <div className="bg-red-500 w-[100vw] h-[100vh]">
-    <div className=" w-full h-[100vh] mt-2 ml-2">
-      <div className="flex items-center gap-6 mb-6">
+    <div className=" w-100% h-[100vh] mt-2 mr-4">
+      <div className="flex items-center gap-4 mb-4">
         <div className="">
-          <div className="bg-red-500 w-[300px] h-[100px] rounded-md mb-6">
+          <div className="bg-red-500 w-[307px] h-[110px] rounded-md mb-4">
             amounnt
           </div>
-          <div className="bg-red-500 w-[300px] h-[100px] rounded-md">
+          <div className="bg-red-500 w-[307px] h-[110px] rounded-md">
             expenses
           </div>
         </div>
-        <div className="bg-red-500 w-[418px] h-[218px] rounded-md">history</div>
-        <div className="bg-red-500 w-[418px] h-[218px] rounded-md">popular</div>
+        <div className="bg-red-500 w-[418px] h-[238px] rounded-md">history</div>
+        <div className="bg-red-500 w-[418px] h-[238px] rounded-md">popular</div>
       </div>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4">
         <div className="bg-red-500 w-[580px] h-[280px] rounded-md">chart 1</div>
         <div className="bg-red-500 w-[580px] h-[280px] rounded-md">chart 2</div>
       </div>
     </div>
   );
 }
+
--- budget/src/components/global/Sidebar.jsx
+++ budget/src/components/global/Sidebar.jsx
@@ -6,9 +6,9 @@ import { MdHomeFilled } from "react-icons/md";
 import { GiWallet } from "react-icons/gi";
 export default function Sidebar() {
   return (
-    <div className=" w-[10vw] h-[100vh] flex flex-col justify-between items-center bg-gray-500 mr-4">
-      <GiWallet className="text-3xl mt-2 cursor-pointer" />
-      <div className="flex flex-col justify-between h-[400px]  text-2xl cursor-pointer mb-16">
+    <div className=" w-[150px] h-[100vh] flex flex-col justify-between items-center bg-gray-500 mr-4">
+      <GiWallet className="text-3xl mt-4 cursor-pointer" />
+      <div className="flex flex-col justify-between h-[400px]  text-2xl cursor-pointer ">
         <div className="flex items-center gap-2">
           <MdHomeFilled />
           <span className="text-sm ">Dashboard</span>
@@ -25,10 +25,10 @@ export default function Sidebar() {
           <IoMdCalendar />
           <span className="text-sm">Calendar</span>
         </div>
-        <div className="flex items-center gap-2">
-          <IoMdSettings />
-          <span className="text-sm">Settings</span>
-        </div>
+      </div>
+      <div className="flex items-center gap-2 text-2xl cursor-pointer mb-4">
+        <IoMdSettings />
+        <span className="text-sm">Settings</span>
       </div>
     </div>
   );
