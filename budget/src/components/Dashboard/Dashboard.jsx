f68ced0d34c221dc9477e78d134698ff11285046
mounir <marnissimounir05@gmail.com>
2024-03-23 17:50:49 +0100

    add responsive desing

--- budget/src/components/Dashboard/Dashboard.jsx
+++ budget/src/components/Dashboard/Dashboard.jsx
@@ -21,7 +21,7 @@
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col w-full  md:mx-0 my-2 ">
+    <div className="flex flex-col w-full  md:mx-0 my-2 bg-red-500 ">
       <div className="flex items-center  md:gap-2 mb-2  flex-col  md:flex-row">
         <div className="flex md:flex-col flex-row gap-2 md:gap-0 w-[100%] md:w-[30%] max-w-[500px] md:max-w-none">
           <div className="bg-red-500 md:w-[100%] w-[50%] h-[120px]  mb-2 rounded-md">
