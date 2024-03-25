279cd9a3635ec3b8fbc7816b6d22cf79094865b9
mounir <marnissimounir05@gmail.com>
2024-03-25 17:29:38 +0100

    add reat routing

--- budget/src/App.jsx
+++ budget/src/App.jsx
@@ -1,9 +1,20 @@
 import Layout from "./components/scene/Layout";
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  
+]);
 function App() {
   return (
     <div className="App text-white ">
-      <Layout />
+        <div className="App text-white">
+          <Layout />
+          <RouterProvider router={router} />
+        </div>
     </div>
   );
 }
