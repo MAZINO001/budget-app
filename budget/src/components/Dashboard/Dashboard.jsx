0b4d550c94d244b07233f64492e14d8b8c486964
mounir <marnissimounir05@gmail.com>
2024-03-25 16:30:53 +0100

    add responsive desing

--- budget/src/components/Dashboard/Dashboard.jsx
+++ budget/src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col w-full md:mx-0 px-2 mb-2">
       <div className="flex items-center  md:gap-2 mb-2  flex-col  md:flex-row ">
-        <div className="flex md:flex-col flex-row gap-2 md:gap-0 w-[100%] md:w-[30%] max-w-[500px] md:max-w-none">
+        <div className="flex md:flex-col flex-row gap-2 md:gap-0 w-[100%] md:w-[30%] max-w-[600px] md:max-w-none">
           <div className="bg-red-500 md:w-[100%] w-[50%] h-[120px]  mb-2 rounded-md">
             amounnt
           </div>
@@ -10,18 +10,18 @@ export default function Dashboard() {
             expenses
           </div>
         </div>
-        <div className="bg-red-500 w-[100%] md:w-[35%] h-[250px] max-w-[500px] md:max-w-none rounded-md md:mb-0 mb-2">
+        <div className="bg-red-500 w-[100%] md:w-[35%] h-[250px] max-w-[600px] md:max-w-none rounded-md md:mb-0 mb-2">
           history
         </div>
-        <div className="bg-red-500 w-[100%] md:w-[35%] h-[250px] max-w-[500px] md:max-w-none rounded-md">
+        <div className="bg-red-500 w-[100%] md:w-[35%] h-[250px] max-w-[600px] md:max-w-none rounded-md">
           popular
         </div>
       </div>
       <div className="flex items-center md:flex-row flex-col gap-2 ">
-        <div className="bg-red-500 w-[100%] md:w-[50%] h-[290px] rounded-md max-w-[500px] md:max-w-none">
+        <div className="bg-red-500 w-[100%] md:w-[50%] h-[290px] rounded-md max-w-[600px] md:max-w-none">
           chart 1
         </div>
-        <div className="bg-red-500 w-[100%] md:w-[50%] h-[290px] rounded-md max-w-[500px] md:max-w-none">
+        <div className="bg-red-500 w-[100%] md:w-[50%] h-[290px] rounded-md max-w-[600px] md:max-w-none">
           chart 2
         </div>
       </div>
--- budget/src/components/global/Sidebar.jsx
+++ budget/src/components/global/Sidebar.jsx
@@ -4,11 +4,35 @@ import { MdHistory } from "react-icons/md";
 import { IoMdCalendar } from "react-icons/io";
 import { MdHomeFilled } from "react-icons/md";
 import { GiWallet } from "react-icons/gi";
+import { useEffect, useState } from "react";
 export default function Sidebar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 640) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className=" md:w-[50px] lg:w-[150px] h-[45px] max-h-[600px] md:h-[100vh] flex flex-row md:flex-col justify-between items-center bg-gray-500 sm:bg-blue-500 md:bg-green-500 lg:bg-yellow-500">
-      <GiWallet className="text-3xl md:ml-0 ml-2 md:mt-2 cursor-pointer" />
-      <div className="flex flex-row md:flex-col justify-between md:h-[400px] text-2xl lg:text-2xl md:text-3xl cursor-pointer gap-14 mx-4">
+    <div className=" md:w-[50px] lg:w-[150px] h-[45px] max-h-[600px] md:h-[100vh] flex flex-row md:flex-col justify-between items-center">
+      <div className="flex items-center gap-2 md:ml-0 ml-2 md:mt-2">
+        <GiWallet className=" text-3xl  cursor-pointer" />
+        <span className="text-lg  none md:hidden lg:flex flex">CashCare</span>
+      </div>
+      <div
+        className={`${
+          isMobile
+            ? "flex flex-row md:flex-col justify-between md:h-[400px] text-3xl cursor-pointer gap-14 mobileSidebar "
+            : "flex flex-row md:flex-col justify-between md:h-[400px] text-3xl cursor-pointer gap-14 mx-4 "
+        }`}
+      >
         <div className="flex items-center gap-2">
           <MdHomeFilled />
           <span className="text-sm none hidden lg:flex">Dashboard</span>
@@ -26,7 +50,7 @@ export default function Sidebar() {
           <span className="text-sm hidden lg:flex">Calendar</span>
         </div>
       </div>
-      <div className="flex items-center gap-2 text-2xl lg:text-2xl md:text-3xl cursor-pointer md:mb-2 md:mr-0 mr-2 ">
+      <div className="flex items-center gap-2 text-3xl cursor-pointer md:mb-2 md:mr-0 mr-2 ">
         <IoMdSettings />
         <span className="text-sm hidden lg:flex">Settings</span>
       </div>
