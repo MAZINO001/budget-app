d49f7b5f28fc4e6cc513896fafac75bd5cc7dc40
mounir <marnissimounir05@gmail.com>
2024-03-26 23:11:06 +0100

    adding charts

--- budget/src/components/Dashboard/Charts.jsx
+++ budget/src/components/Dashboard/Charts.jsx
@@ -1,44 +1,68 @@
-import { Radar } from "react-chartjs-2";
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const data = [
   {
-    label: "Skill 1",
-    data: [85, 75, 90, 80, 70],
-    backgroundColor: "rgba(255, 99, 132, 0.2)",
-    borderColor: "rgba(255, 99, 132, 1)",
-    pointBackgroundColor: "rgba(255, 99, 132, 1)",
-    pointBorderColor: "#fff",
-    pointHoverRadius: 5,
-    pointHitRadius: 10,
-    pointBorderWidth: 2,
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
   },
   {
-    label: "Skill 2",
-    data: [70, 80, 60, 90, 85],
-    backgroundColor: "rgba(54, 162, 235, 0.2)",
-    borderColor: "rgba(54, 162, 235, 1)",
-    pointBackgroundColor: "rgba(54, 162, 235, 1)",
-    pointBorderColor: "#fff",
-    pointHoverRadius: 5,
-    pointHitRadius: 10,
-    pointBorderWidth: 2,
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
   },
 ];
 
-const RadarChart = () => {
-  const options = {
-    scale: {
-      pointLabels: {
-        fontSize: 14,
-      },
-    },
-  };
+export default function RadraChart() {
+  //   static demoUrl = 'https://codesandbox.io/s/tiny-line-chart-r5z0f';
 
   return (
-    <div>
-      <Radar data={data} options={options} />
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={300} height={100} data={data}>
+        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+      </LineChart>
+    </ResponsiveContainer>
   );
-};
-
-export default RadarChart;
+}
