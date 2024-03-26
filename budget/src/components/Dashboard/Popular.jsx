4bdbf88669e097bf31154c21efe88908fbffae51
mounir <marnissimounir05@gmail.com>
2024-03-26 21:54:30 +0100

    dashboard desing

--- budget/src/components/Dashboard/Popular.jsx
+++ budget/src/components/Dashboard/Popular.jsx
@@ -1,14 +1,47 @@
+import { IoMdRestaurant } from "react-icons/io";
+
 export default function Popular() {
   return (
-    <div className="">
-      <h3 className="pb-1 text-lg font-semibold tracking-wider">Popular</h3>
+    <div className=" ">
+      <h3 className="pb-1 text-lg font-semibold tracking-wider ">Popular</h3>
       <hr className="pb-2" />
-      <div className="flex flex-col gap-2 items-center ">
-        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
-        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
-        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
-        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
-        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+      <div className="flex flex-col gap-2 items-center">
+        <div className="bg-gray-700 w-[100%] h-[46px] rounded-md px-2 flex items-center justify-between">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-700 w-[100%] h-[46px] rounded-md px-2 flex items-center justify-between">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-700 w-[100%] h-[46px] rounded-md px-2 flex items-center justify-between">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-700 w-[100%] h-[46px] rounded-md px-2 flex items-center justify-between">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
       </div>
     </div>
   );
