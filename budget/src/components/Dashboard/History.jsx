5f8b8efbedf9f5f60d41ef9d9c5a55e854e46a6e
mounir <marnissimounir05@gmail.com>
2024-03-26 15:03:49 +0100

    dashboard desing

---
+++ budget/src/components/Dashboard/History.jsx
@@ -0,0 +1,15 @@
+export default function History() {
+  return (
+    <div className="">
+      <h3 className="pb-1 text-lg font-semibold tracking-wider">History</h3>
+      <hr className="pb-2" />
+      <div className="flex flex-col gap-2 items-center ">
+        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+        <div className="bg-blue-500 w-[97%] h-[35px] rounded-md pl-2">test</div>
+      </div>
+    </div>
+  );
+}
