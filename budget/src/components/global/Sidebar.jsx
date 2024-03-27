6f161001d37843aa2a0e143a5a89dce22ff166e7
mounir <marnissimounir05@gmail.com>
2024-03-27 22:48:03 +0100

    assing history desing

--- budget/src/components/global/Sidebar.jsx
+++ budget/src/components/global/Sidebar.jsx
@@ -65,7 +65,7 @@
 import { MdOutlineAddBox } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
 import { MdHistory } from "react-icons/md";
-import { LiaToolsSolid } from "react-icons/lia";
+import { FaTools } from "react-icons/fa";
 import { MdHomeFilled } from "react-icons/md";
 import { GiWallet } from "react-icons/gi";
 import { useEffect, useState } from "react";
@@ -132,11 +132,11 @@ export default function Sidebar() {
         <Link
           to="/tools"
           className={`flex items-center gap-2 ${
-            location.pathname === "/Tools" ? "text-red-500" : ""
+            location.pathname === "/tools" ? "text-red-500" : ""
           }`}
         >
-          <LiaToolsSolid  className="text-3xl" />
-          <span className="text-sm hidden lg:flex">Tools</span>
+          <FaTools  className="text-2xl"/>
+          <span className="text-sm hidden lg:flex">Calendar</span>
         </Link>
       </div>
 
--- budget/src/components/pages/Tools.jsx
+++ budget/src/components/pages/Tools.jsx
@@ -1,5 +1,10 @@
-export default function Canedar() {
+export default function Tools() {
   return (
-    <div>Canedar</div>
-  )
+    <div className="grid grid-cols-2 grid-rows-2 gap-2 mx-2 mb-2 h-[100%] ">
+      <div className="bg-gray-700 rounded-md">test tools</div>
+      <div className="bg-gray-700 rounded-md">test tools</div>
+      <div className="bg-gray-700 rounded-md">test tools</div>
+      <div className="bg-gray-700 rounded-md">test tools</div>
+    </div>
+  );
 }
