b870042250f606b5ff83734a3c6de96316fa438a
mounir <marnissimounir05@gmail.com>
2024-03-27 22:59:19 +0100

    adding addnew desing

--- budget/src/components/pages/AddNew.jsx
+++ budget/src/components/pages/AddNew.jsx
@@ -1,5 +1,15 @@
 export default function AddNew() {
   return (
-    <div>AddNew</div>
-  )
+    <div className="flex flex-col  mx-2">
+      <div className="bg-gray-700 h-24 rounded-md flex justify-evenly items-center">
+        <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add transction</button>
+        <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add budget</button>
+        <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add davings</button>
+      </div>
+
+      <div class>
+
+      </div>
+    </div>
+  );
 }
