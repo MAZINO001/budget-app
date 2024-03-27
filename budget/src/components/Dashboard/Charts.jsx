7f04289de38e4e6200f2f37b9f64dc01ec8032b4
mounir <marnissimounir05@gmail.com>
2024-03-27 23:12:20 +0100

    adding History desing

--- budget/src/components/Dashboard/Charts.jsx
+++ budget/src/components/Dashboard/Charts.jsx
@@ -12,6 +12,11 @@ import {
   CartesianGrid,
   XAxis,
   YAxis,
+  BarChart,
+  Tooltip,
+  Legend,
+  Bar,
+  Rectangle,
 } from "recharts";
 
 const data1 = [
@@ -98,6 +103,51 @@ const data2 = [
   },
 ];
 
+const data3 = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 export function ChartLines(interval) {
   return (
     <ResponsiveContainer height="100%" width="100%">
@@ -136,12 +186,29 @@ export function ChartRadar() {
     </ResponsiveContainer>
   );
 }
-// export function ChartLines() {
-//   return (
-//     <ResponsiveContainer width="100%" height="100%">
-//       <LineChart width={300} height={100} data={data1}>
-//         <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-//       </LineChart>
-//     </ResponsiveContainer>
-//   );
-// }
+export function ChartBars() {
+  return (
+  <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data3}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#0000ff" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+          <Bar dataKey="uv" fill="#00ff00" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+          <Bar dataKey="amt" fill="#ff0000" activeBar={<Rectangle fill="red" stroke="red" />} />
+        </BarChart>
+      </ResponsiveContainer>
+  );
+}
--- budget/src/components/pages/AddNew.jsx
+++ budget/src/components/pages/AddNew.jsx
@@ -1,14 +1,16 @@
+import { ChartBars } from "../Dashboard/Charts";
+
 export default function AddNew() {
   return (
-    <div className="flex flex-col  mx-2">
-      <div className="bg-gray-700 h-24 rounded-md flex justify-evenly items-center">
+    <div className="flex flex-col mb-2 mx-2 h-[100%]">
+      <div className="bg-gray-700 h-[20%] rounded-md flex justify-evenly items-center mb-2">
         <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add transction</button>
         <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add budget</button>
         <button className="bg-red-500 p-3 rounded-md text-lg capitalize font-semibold tracking-wider cursor-pointer">add davings</button>
       </div>
 
-      <div class>
-
+      <div className="bg-gray-700 h-[80%] rounded-md">
+        <ChartBars/>
       </div>
     </div>
   );
