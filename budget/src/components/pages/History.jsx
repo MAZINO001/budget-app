c0028fad17c9908a1242ff4c77d62a4bb8ea2708
mounir <marnissimounir05@gmail.com>
2024-03-28 18:05:44 +0100

    adding History desing

--- budget/src/components/pages/History.jsx
+++ budget/src/components/pages/History.jsx
@@ -1,5 +1,129 @@
+import { IoMdRestaurant } from "react-icons/io";
 export default function History() {
   return (
-    <div>History</div>
-  )
+    <div className="mx-2">
+      <div className="bg-gray-700 rounded-md h-[3rem] mb-1 text-lg font-semibold tracking-wider flex px-2 justify-between items-center text-center capitalize">
+        <span className="bg-gray-600 w-[6rem] h-[2rem] rounded-md ">
+          category
+        </span>
+        <span className="bg-gray-600 w-[6rem] h-[2rem] rounded-md ">Type</span>
+        <span className="bg-gray-600 w-[6rem] h-[2rem] rounded-md ">Date</span>
+        <span className="bg-gray-600 w-[6rem] h-[2rem] rounded-md ">Time</span>
+        <span className="bg-gray-600 w-[6rem] h-[2rem] rounded-md ">
+          Amount
+        </span>
+      </div>
+      <div className="bg-gray-700 rounded-md h-[82vh] p-2 space-y-2 overflow-auto">
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        {/* testign */}
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+        <div className="bg-gray-600 w-[100%] h-[4rem] rounded-md px-2 flex items-center justify-between ">
+          <div className="flex">
+            <IoMdRestaurant className="text-[28px] text-red-500 border-red-500 rounded-full border" />
+            <span className="ml-2">Food</span>
+          </div>
+          <div>
+            <span>200$</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
