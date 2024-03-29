3d1886ea78850ca92081f843e657c3aff1fc399c
mounir <marnissimounir05@gmail.com>
2024-03-29 16:24:20 +0100

    fixing some desing errors

--- budget/src/components/pages/AddNew.jsx
+++ budget/src/components/pages/AddNew.jsx
@@ -1,3 +1,27 @@
+// import { ChartBars } from "../Dashboard/Charts";
+
+// export default function ew() {
+//   return (
+//     <div className="flex flex-col mb-2 mx-2 h-[89vh] ">
+//       <div className="bg-gray-700 h-[4rem] rounded-md flex justify-between items-center mb-2 px-2">
+//         <button className="bg-gray-600 p-2 rounded-md md:text-lg text-md capitalize font-semibold tracking-wider cursor-pointer">
+//           transction
+//         </button>
+//         <button className="bg-gray-600 p-2 rounded-md md:text-lg text-md capitalize font-semibold tracking-wider cursor-pointer">
+//           budget
+//         </button>
+//         <button className="bg-gray-600 p-2 rounded-md md:text-lg text-md capitalize font-semibold tracking-wider cursor-pointer ">
+//           davings
+//         </button>
+//       </div>
+
+//       <div className="bg-gray-700 h-[100%] rounded-md">
+//         <ChartBars />
+//       </div>
+//     </div>
+//   );
+// }
+
 import { ChartBars } from "../Dashboard/Charts";
 
 export default function ew() {
@@ -16,7 +40,10 @@ export default function ew() {
       </div>
 
       <div className="bg-gray-700 h-[100%] rounded-md">
-        <ChartBars />
+        <div className="bg-gray-700"></div>
+        <div className="bg-gray-700">
+          
+        </div>
       </div>
     </div>
   );
--- budget/src/components/pages/Tools.jsx
+++ budget/src/components/pages/Tools.jsx
@@ -1,6 +1,6 @@
 export default function Tools() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 grid-rows-3 gap-2 mx-2 mb-2 h-[90.2vh] ">
+    <div className="grid grid-cols-2 md:grid-cols-2 grid-rows-3 gap-2 mx-2 mb-2 h-[90.2vh] ">
       <div className="bg-gray-700 rounded-md">test tools</div>
       <div className="bg-gray-700 rounded-md">test tools</div>
       <div className="bg-gray-700 rounded-md">test tools</div>
