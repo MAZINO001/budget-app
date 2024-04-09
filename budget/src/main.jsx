2196ad891fb00f24e05e633c73d527c001e52a83
mounir <marnissimounir05@gmail.com>
2024-04-09 15:30:57 +0100

    adding functionality to the app

--- budget/src/main.jsx
+++ budget/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { PrimeReactProvider } from "primereact/api";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./components/Redux/Store.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <Provider store={store}>
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </Provider>
   </React.StrictMode>
 );
