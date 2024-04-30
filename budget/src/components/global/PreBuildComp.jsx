f60ceb96ee75c04443af945f924e74af312ad35a
mounir <marnissimounir05@gmail.com>
2024-04-30 18:30:54 +0000

    fix design errors

--- budget/src/components/global/PreBuildComp.jsx
+++ budget/src/components/global/PreBuildComp.jsx
@@ -3,7 +3,8 @@
 import "primereact/resources/themes/tailwind-light/theme.css";
 import { Dropdown } from "primereact/dropdown";
 import { useState } from "react";
-import Data from "../../Data/fakeData.json";
+import { updatedJsonData } from "../Redux/Features/AddNewSlices/TransactionSlice";
+// import Data from "../../Data/fakeData.json";
 
 export function Rnage() {
   const [selectedCity, setSelectedCity] = useState(null);
@@ -28,7 +29,8 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 export default function HistoryPreBiuld1() {
-  const [transactions, setTransactions] = useState(Data);
+  // const [transactions, setTransactions] = useState(Data);
+  const [transactions, setTransactions] = useState(updatedJsonData);
 
   return (
     <div className="card " style={{ backgroundColor: "#000" }}>
